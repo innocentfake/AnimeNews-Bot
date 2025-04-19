fa426a3866f95334a5e69e8a1e3ad0c47531ceb4
Nadan parinda <sukalalthakare19@gmail.com>
2025-04-19 12:30:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ BOT_TOKEN = "7957115059:AAHJ8yL-4wEOhc5PspgpuS7wz7fSbbK2qKA" #Bot Token get it f
 URL_A = "https://myanimelist.net/" #News RSS Feed Url 1
 URL_B = "https://www.crunchyroll.com" #News RSS Feed Url 2
 START_PIC = "http://postimg.cc/68vfZ3c4" #Startup Pic
-MONGO_URI = "mongodb+srv://thakareankit2005:<Asthakare#123>@animenews.trqlvgv.mongodb.net/?retryWrites=true&w=majority&appName=Animenews" #Databse uri get it from https://www.mongodb.com/
+MONGO_URI = "mongodb://atlas-sql-68033317b110aa01cc7f7b0a-kx4iu.a.query.mongodb.net/Animenews?ssl=true&authSource=admin" #Databse uri get it from https://www.mongodb.com/
 ADMINS = "5455664988" #Admin ID
