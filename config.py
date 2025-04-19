72c4ef452732434d90a66f9687aead19d3aee5dc
Nadan parinda <sukalalthakare19@gmail.com>
2025-04-19 10:45:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@
 API_ID = "" #API ID get it from telegram.org
 API_HASH = "" #API HASH get it from telegram.org
 BOT_TOKEN = "" #Bot Token get it from @BotFather
-URL_A = "" #News RSS Feed Url 2
-#URL_B = "" #News RSS Feed Url 2
-START_PIC = "" #Startup Pic
+URL_A = "http://myanimelist.net/rss.php?type=news" #News RSS Feed Url 2
+URL_B = "https://www.crunchyroll.com" #News RSS Feed Url 2
+START_PIC = "http://postimg.cc/68vfZ3c4" #Startup Pic
 MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
+ADMINS = "" #Admin ID
