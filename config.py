4f50691265092937783ae85b7c39545b35518365
Nadan parinda <sukalalthakare19@gmail.com>
2025-04-19 11:27:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ BOT_TOKEN = "" #Bot Token get it from @BotFather
 URL_A = "http://myanimelist.net/rss.php?type=news" #News RSS Feed Url 2
 URL_B = "https://www.crunchyroll.com" #News RSS Feed Url 2
 START_PIC = "http://postimg.cc/68vfZ3c4" #Startup Pic
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
+MONGO_URI = "mongodb+srv://thakareankit2005:<db_password>@animenews.trqlvgv.mongodb.net/?retryWrites=true&w=majority&appName=Animenews" #Databse uri get it from https://www.mongodb.com/
 ADMINS = "" #Admin ID
